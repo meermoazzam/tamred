eb356f5a3276a4240c83b1b77cf987f034f5400e
Meer Moazzam <meer.moazzam@purelogics.net>
2023-12-06 18:59:33 +0500

    initial  commit

---
+++ app/Models/UserMeta.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserMeta extends Model
+{
+    use HasFactory;
+
+    protected $table = 'user_meta';
+
+    protected $fillable = [
+        "user_id",
+        "meta_key",
+        "meta_value",
+    ];
+}
